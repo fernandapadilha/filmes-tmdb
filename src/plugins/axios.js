3f66aeda879aa9ffd3b18de259ab7079030592e7
ViniRech <viniciusrech2007@gmail.com>
2024-11-25 11:00:40 -0300

    continuação do tutorial

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `Bearer COLOQUE AQUI A CHAVE DE LEITURA DA API`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjU4MThjOTg2NWM1NDFiMzExZDRiNDEwN2MwM2Q3MCIsIm5iZiI6MTczMjUzODY3OC42MDE4OTQxLCJzdWIiOiI2NzM0YmJlN2M5NzI3ODFiOGQ3M2Q1ZDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.PgA5cx9m-0rEiOIRmbqeU7Mbprjfls0vk0XKOhgAW6k`,
   },
 });
 
--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,22 +1,60 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue';
+import api from '@/plugins/axios';
+import Loading from 'vue-loading-overlay';
+import { useGenreStore } from '@/stores/genre'
 
-  const genres = ref([]);
+const genreStore = useGenreStore();
+const tvShows = ref([]);
+const isLoading = ref(false);
 
-  onMounted(async () => {
-    const response = await api.get('genre/tv/list?language=pt-BR');
-    genres.value = response.data.genres;
+onMounted(async () => {
+  isLoading.value = true;
+  await genreStore.getAllGenres('movie');
+  isLoading.value = false;
+});
+
+const listTvShows = async (genreId) => {
+  isLoading.value = true;
+  const response = await api.get('discover/tv', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
   });
+  tvShows.value = response.data.results
+  isLoading.value = false;
+};
+
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
 </script>
 
 <template>
     <h1>Programas de TV</h1>
     <ul class="genre-list">
-      <li v-for="genre in genres" :key="genre.id" class="genre-item">
-        {{ genre.name }}
+      <li v-for="genre in genreStore.genres" :key="genre.id" @click="listMovies(genre.id)" class="genre-item">
+        {{ genre.name }} 
       </li>
     </ul>
+
+    <loading v-model:active="isLoading" is-full-page />
+
+    <div class="tv-list">
+      <div v-for="tvShow in tvShows" :key="tv.id" class="tv-card">
+              
+        <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+        <div class="tv-details">
+          <p class="tv-title">{{ movie.title }}</p>
+          <p class="tv-release-date">{{ formatDate(movie.release_date) }}</p>
+          <p class="tv-genres">
+            <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
+              {{ genreStore.getGenreName(genre_id) }}
+            </span>
+          </p>
+        </div>
+              
+      </div>
+    </div>
 </template>
   
   <style scoped>
