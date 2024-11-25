418ad29c84962ebeae0123d60039e378731041c4
ViniRech <viniciusrech2007@gmail.com>
2024-11-25 16:48:53 -0300

    Início do exercício

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -10,7 +10,7 @@ const isLoading = ref(false);
 
 onMounted(async () => {
   isLoading.value = true;
-  await genreStore.getAllGenres('movie');
+  await genreStore.getAllGenres('tv');
   isLoading.value = false;
 });
 
@@ -32,7 +32,7 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
 <template>
     <h1>Programas de TV</h1>
     <ul class="genre-list">
-      <li v-for="genre in genreStore.genres" :key="genre.id" @click="listMovies(genre.id)" class="genre-item">
+      <li v-for="genre in genreStore.genres" :key="genre.id" @click="listTvShows(genre.id)" class="genre-item">
         {{ genre.name }} 
       </li>
     </ul>
@@ -40,14 +40,14 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
     <loading v-model:active="isLoading" is-full-page />
 
     <div class="tv-list">
-      <div v-for="tvShow in tvShows" :key="tv.id" class="tv-card">
+      <div v-for="tvShow in tvShows" :key="tvShow.id" class="tv-card">
               
-        <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+        <img :src="`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`" :alt="tvShow.title" />
         <div class="tv-details">
-          <p class="tv-title">{{ movie.title }}</p>
-          <p class="tv-release-date">{{ formatDate(movie.release_date) }}</p>
+          <p class="tv-title">{{ tvShow.name }}</p>
+          <p class="tv-release-date">{{ formatDate(tvShow.air_date) }}</p>
           <p class="tv-genres">
-            <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
+            <span v-for="genre_id in tvShow.genre_ids" :key="genre_id" @click="listTvShows(genre_id)">
               {{ genreStore.getGenreName(genre_id) }}
             </span>
           </p>
@@ -58,28 +58,81 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
 </template>
   
   <style scoped>
-    .genre-list {
-      display: flex;
-      justify-content: center;
-      flex-wrap: wrap;
-      gap: 2rem;
-      list-style: none;
-      padding: 0;
-    }
-  
-    .genre-item {
-      background-color: #5d6424;
-      border-radius: 1rem;
-      padding: 0.5rem 1rem;
-      align-self: center;
-      color: #fff;
-      display: flex;
-      justify-content: center;
-    }
-  
-    .genre-item:hover {
-      cursor: pointer;
-      background-color: #7d8a2e;
-      box-shadow: 0 0 0.5rem #5d6424;
-    }
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
+}
+
+.genre-item {
+  background-color: #387250;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
+}
+
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #4e9e5f;
+  box-shadow: 0 0 0.5rem #387250;
+}
+
+.tv-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tv-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-details {
+  padding: 0 0.5rem;
+}
+
+.tv-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}   
+
+.tv-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.tv-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.tv-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
+}
   </style>
